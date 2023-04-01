5cac2c177d7c0ccb094d9b16f2752b82dfc380b1
Abimael M <abimaelmercado05@gmail.com>
2023-03-31 22:14:33 -0400

    more optimization

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,12 +6,12 @@ function About() {
     <div className="h-fit w-full text-center py-20 mt-16" id="about">
       <div className=" md:w-5/6 w-fit min-h-40 bg-gradient-to-br from-gray-600 to-gray-300 shadow-xl duration-500 rounded-lg hover:rounded-3xl text-gray-200 p-6 py-8 mx-auto">
         <h1 className="md:text-2xl text-xl font-medium mb-2">About Me</h1>
-        <h3 className="md:text-2xl text-xl leading-relaxed">
+        <h2 className="md:text-2xl text-xl leading-relaxed">
           Hello there, I&rsquo;m Abimael Mercado. A
           <span className=" font-extralight"> software developer</span> with a
           hospitality, and operations background. Passionate about continuous
           learning and always strive to improve my skills.
-        </h3>
+        </h2>
         <div className="flex justify-center text-4xl mt-4 space-x-6">
           <a
             href="https://www.github.com/mercad0"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import About from "@/components/About";
 import NavBar from "@/components/navbar/Navbar";
 import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="w-full flex justify-center bg-gradient-to-br from-gray-800 to-gray-400 h-full pb-20">
+       <Head>
+        <title>Abimael M / Software Developer</title>
+        <meta name="description" content="Abimael M Portfolio - front-end Software Developer, Explore recent projects, and contact information." />
+        <link rel="icon" href="/images/tabIcon.png" />
+      </Head>
       <NavBar />
       <div className="flex-grow mx-6">
         <div className="flex">
